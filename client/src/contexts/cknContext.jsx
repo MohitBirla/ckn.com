ddc94cb295d838d5f975e91724be18e5f802e534
Mohit <88202920+MohitBirla@users.noreply.github.com>
2023-07-08 00:00:32 +0530

    new

--- client/src/contexts/cknContext.jsx
+++ client/src/contexts/cknContext.jsx
@@ -13,7 +13,8 @@ export default function CknContext({ children }) {
   const [showOrder, setShowOrder] = useState(false);
   const [token, setToken] = useState("");
   // const [URL, setURL] = useState("http://localhost:8080/api");
-  const [URL, setURL] = useState("https://chai-ke-nashedi.onrender.com/api");
+  // const [URL, setURL] = useState("https://chai-ke-nashedi.onrender.com/api");
+  const [URL, setURL] = useState("https://chaikenashedi-com.onrender.com/api");
 
   const [chai, setChai] = useState(false);
   const [chaiAmount, setChaiAmount] = useState(0);
